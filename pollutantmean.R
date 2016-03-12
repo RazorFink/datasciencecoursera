36ca60d8264266a0a682486d08a78d11e9714928
RazorFink <mhowemegis@gmail.com>
2016-03-12 12:22:31 -0500

    Week 2 Programming Assignment Part 1

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,7 +2,6 @@
 pollutantmean <- function(directory, pollutant, id = 1:332){
 #'directory' is a vector of character length 1 
 #indicating the location of the CSV files.
-  
 #'pollutant' is a character vector of length 1.  
 #it will contain the name of the pollutant for which
 #we will calculate the mean; either 'nitrate' or 'sulfate'.
@@ -13,5 +12,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
 #Return the mean of the pollutant values across all monitors 
 #listed in the id vector (excluding NA values)
 #NOTE: Don't round the result!
-p <- read.csv(directory)
+    filenames <- dir(directory, full.names = TRUE)[id]
+    pVecs <- lapply(filenames, function(f){read.csv(f)[[pollutant]]})
+    mean(unlist(pVecs), na.rm = TRUE)
 }
