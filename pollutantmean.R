388b6ce2b97d2957e001fde86f42c34452c44ad1
RazorFink <mhowemegis@gmail.com>
2016-03-12 08:27:52 -0500

    start pollutantmeat function

---
+++ pollutantmean.R
@@ -0,0 +1,17 @@
+# Function pollutantmean
+pollutantmean <- function(directory, pollutant, id = 1:332){
+#'directory' is a vector of character length 1 
+#indicating the location of the CSV files.
+  
+#'pollutant' is a character vector of length 1.  
+#it will contain the name of the pollutant for which
+#we will calculate the mean; either 'nitrate' or 'sulfate'.
+  
+#'id' is an integer vector of length 1 indicating the monitor
+#ID numbers to be used.
+  
+#Return the mean of the pollutant values across all monitors 
+#listed in the id vector (excluding NA values)
+#NOTE: Don't round the result!
+p <- read.csv(directory)
+}
